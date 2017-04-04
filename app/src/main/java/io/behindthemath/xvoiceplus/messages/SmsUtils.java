f7f2ffcd347adae2eefb487042f118f7daa418a2
Behind The Math <info@behindthemath.io>
2017-04-03 21:18:09 -0400

    Refactor hexArray from a static field to a local variable

--- app/src/main/java/io/behindthemath/xvoiceplus/messages/SmsUtils.java
+++ app/src/main/java/io/behindthemath/xvoiceplus/messages/SmsUtils.java
@@ -105,8 +105,8 @@ public class SmsUtils {
         return result;
     }
 
-    private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
     private static String bytesToHex(byte... bytes) {
+        final char[] hexArray = "0123456789ABCDEF".toCharArray();
         char[] hexChars = new char[bytes.length * 2];
         for ( int j = 0; j < bytes.length; j++ ) {
             int v = bytes[j] & 0xFF;
